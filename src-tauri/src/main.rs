9f3c32ffa560456f0ebf114e21ac968b99402f27
sujang.958 <enter7377@naver.com>
2023-07-23 17:26:22 +0900

    feat: implement capturing keyboard inputs

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,43 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::Manager;
+use device_query::{DeviceQuery, DeviceState, Keycode};
+use std;
+
+#[derive(Clone, serde::Serialize)]
+struct Payload {
+  key: String,
+}
+
+fn main() {
+  tauri::Builder::default()
+    .setup(|app| {
+      let main_window = app.get_window("main").unwrap();
+
+      std::thread::spawn(move || {
+        let device_state = DeviceState::new();
+        let mut past_keys: Vec<Keycode> = Vec::new();
+
+        loop {
+          let pressed_keys: Vec<Keycode> = device_state.get_keys();
+          
+          let detached_keys = past_keys.iter().filter(|key| !pressed_keys.contains(key));
+
+          for key in detached_keys {
+            let _ = main_window.emit("keyUp", Payload {key: key.to_string()});
+          }
+          for key in pressed_keys.iter() {
+            let _ = main_window.emit("keyDown", Payload {key: key.to_string()});
+          }
+
+          past_keys = pressed_keys.clone();
+        }
+      });
+    
+      Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
+
---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { listen, type UnlistenFn } from "@tauri-apps/api/event"
+  import { onDestroy } from "svelte"
+
+  const unlistens: UnlistenFn[] = []
+
+  listen("keyDown", (event) => {
+    console.log("Down", event.payload)
+  }).then((v) => unlistens.push(v))
+
+  listen("keyUp", (event) => {
+    console.log("Up", event.payload)
+  }).then((v) => unlistens.push(v))
+
+  onDestroy(() => {
+    unlistens.forEach((v) => v())
+  })
+</script>
+
+<h1>hi</h1>
